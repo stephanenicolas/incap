080499d51df9756d50667feee1868ffd7820369c
Stéphane Nicolas <steff.nicolas@gmail.com>
2017-06-30 11:24:40 -0700

    2nd test

--- core/src/test/java/org/gradle/incap/impl/data/StateGraphTest.java
+++ core/src/test/java/org/gradle/incap/impl/data/StateGraphTest.java
@@ -10,7 +10,7 @@ public class StateGraphTest {
   StateGraph stateGraphUnderTest;
 
   @org.junit.Test
-  public void testAddGenerationEdge() throws Exception {
+  public void testAddGenerationEdge_shouldReturnSingleParticipatingElement() throws Exception {
     //GIVEN
     stateGraphUnderTest = new StateGraph();
     GeneratedFile generatedFile = new GeneratedSourceFile("foo");
@@ -22,5 +22,21 @@ public class StateGraphTest {
 
     //THEN
     assertThat(participatingElementEntries.size(), is(1));
+  }
+
+  @org.junit.Test
+  public void testAddGenerationEdge_shouldReturnSingleParticipatingElements() throws Exception {
+    //GIVEN
+    stateGraphUnderTest = new StateGraph();
+    GeneratedFile generatedFile = new GeneratedSourceFile("foo");
+    ElementEntry entry1 = new ElementEntry("");
+    ElementEntry entry2 = new ElementEntry("");
+
+    //WHEN
+    stateGraphUnderTest.addGenerationEdge(generatedFile, entry1, entry2);
+    Set<ElementEntry> participatingElementEntries = stateGraphUnderTest.getParticipatingElementEntries(generatedFile);
+
+    //THEN
+    assertThat(participatingElementEntries.size(), is(2));
   }
 }
