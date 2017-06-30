381943a40610552bb993f85779baa926e2aaa79b
Weihua Wang <wewang@groupon.com>
2017-06-30 13:55:01 -0700

    change inputfile to be string name

--- core/src/main/java/org/gradle/incap/impl/data/InputFile.java
+++ core/src/main/java/org/gradle/incap/impl/data/InputFile.java
@@ -1,10 +1,7 @@
 package org.gradle.incap.impl.data;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class InputFile {
 
-  private Set<ElementEntry> elementEntries = new HashSet<>();
+  private String name;
 
 }
