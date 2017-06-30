68ace894489916806d50fe4dc5a63c061001ade1
Stéphane Nicolas <steff.nicolas@gmail.com>
2017-06-30 11:35:31 -0700

    chage rentention

--- annotations/src/main/java/com/example/Annotation1.java
+++ annotations/src/main/java/com/example/Annotation1.java
@@ -1,13 +1,8 @@
 package com.example;
 
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.CLASS;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
 
 @Retention(RUNTIME)
-@Target(TYPE)
 public @interface Annotation1 {}
