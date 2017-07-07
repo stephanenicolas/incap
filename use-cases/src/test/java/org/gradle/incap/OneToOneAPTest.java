a6f869735863e4030b82760edc42a8cc045cbd01
Stéphane Nicolas <steff.nicolas@gmail.com>
2017-07-07 16:04:33 -0700

    add test folder

---
+++ use-cases/src/test/java/org/gradle/incap/OneToOneAPTest.java
@@ -0,0 +1,15 @@
+package org.gradle.incap;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class OneToOneAPTest {
+
+  @Test
+  public void testOneToOneAP_creates_GeneratedFile_WhenSingleFileToProcess() {
+    //GIVEN
+    //WHEN
+    //THEN
+  }
+
+}
